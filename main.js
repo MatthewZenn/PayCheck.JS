e1cdb3decbe343085989be688dccb8c177a40e7d
Matthew Zenn <spyguy57@gmail.com>
2021-09-06 11:39:03 -0400

    replaced alert with inbody text

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 var field = document.getElementById('field');
 var go = document.getElementById('button');
+var text = document.getElementById('message');
 
 go.addEventListener("click", () =>  {
     try {
         Luhn(field.value, 5, 2);
     } 
     catch(err) {
-        alert(err);
+        text.innerHTML = err;
     }   
 });
